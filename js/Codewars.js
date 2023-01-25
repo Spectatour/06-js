a97453d19a273b22b4f60fcba8f208d8728639d4
Spectatour <ekalpokas@gmail.com>
2023-01-25 16:37:52 +0200

    Codewars

--- js/Codewars.js
+++ js/Codewars.js
@@ -1,3 +1,4 @@
+console.clear ();
 console.log (`---------------Sum of positive----------------------`)
 // console.clear ();
 
@@ -61,3 +62,111 @@ console.log (`---------------Polish alphabet----------------------`)
 
 
 //   console.log(correctPolishLetters(`Jędrzej Błądziński`));
+
+console.log (`---------------Get Planet Name By ID----------------------`)
+
+// function getPlanetName(id){         // reikalingas break po kiekvieno case. 
+//     let name;                       //  kad ciklas sustoju prie tam tikros dienos
+//     switch(id){
+//       case 1:
+//         name = 'Mercury'
+//         break;
+//       case 2:
+//         name = 'Venus'
+//         break;
+//       case 3:
+//         name = 'Earth'
+//         break;
+//       case 4:
+//         name = 'Mars'
+//         break;
+//       case 5:
+//         name = 'Jupiter'
+//         break;
+//       case 6:
+//         name = 'Saturn'
+//         break;
+//       case 7:
+//         name = 'Uranus'
+//         break;
+//       case 8:
+//         name = 'Neptune'
+//         break;
+//     }
+    
+//     return name;
+//   }
+
+//   console.log (getPlanetName(5))
+
+console.log (`---------------You only need one - Beginner----------------------`)
+
+// function check(a, x) {
+//   return a.includes(x);
+// }
+// console.log(check([66, 101], 66), true);
+// console.log(check([101, 45, 75, 105, 99, 107], 107), true);
+// console.log(check(['t', 'e', 's', 't'], 'e'), true);
+// console.log(check(['what', 'a', 'great', 'kata'], 'kat'), false);
+
+console.log (`---------------All Star Code Challenge #18----------------------`)
+
+// function strCount(str, letter) {
+//     let count = 0;
+//     for (let i = 0; i < str.length; i++) {
+//       if (str[i] === letter) {
+//         count++;
+//       }
+//     }
+//     return count;
+//   }
+
+//   console.log(strCount("Hello", "o"));
+//   console.log(strCount("Hello", "l"));
+//   console.log(strCount("", "z"));
+
+console.log (`---------------Is it even?----------------------`)
+
+// const n =10
+
+// function testEven(n) {
+//     return n % 2 === 0;
+// }
+
+// function testEven(n){
+//     return !(n % 2);
+//   }
+
+
+// console.log (testEven(10));
+
+// console.log (testEven(1.5));
+
+// console.log (testEven(4/2));
+
+  console.log (`---------------Is he gonna survive?----------------------`)
+
+//   function hero(bullets, dragons){
+//     if (bullets >= dragons *2) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+//   }
+
+//   console.log(hero(7,3));
+//   console.log(hero(4,3));
+
+
+
+
+
+
+
+
+function reverseWords(str) {
+    let reverseWord = str.split(" ").reverse().join(" ");
+    console.log(reverseWord);
+    return reverseWord;
+  }
+  console.log(reverseWords(`the worlds greatest`));
